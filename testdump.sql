8ecdb45d833ac22cf37b9ce57680074738f25197
ednaldo <ednaldofranco.dev@gmail.com>
2022-01-24 16:53:23 -0300

    removindo arquivos nao utilizado

---
+++ testdump.sql
@@ -0,0 +1,9 @@
+create table people(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY ( id )
+);
+
+insert into people (name) values ("Ednaldo");
+insert into people (name) values ("Isabela");
+insert into people (name) values ("Arthur");
