83f68f3c252de95bfb520f1eb252fce614609c77
ednaldo <ednaldofranco.dev@gmail.com>
2022-01-24 11:15:02 -0300

    first commit

---
+++ node/server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
+  const mysql = require('mysql')
+  const connection = mysql.createConnection(config)
+
+  const sql = `INSERT INTO people(name) values('EDNALDO')`
+  const sqlQuery = `SELECT name FROM people WHERE name='EDNALDO'`
+  connection.query(sql)
+
+  connection.end()
+
+
+app.get('/', (req,res) => {
+  const con = mysql.createConnection(config)
+  con.query('SELECT name FROM people', (err, rows) => {
+  var nomes = ""
+    if (err) throw err
+    rows.forEach(row => {
+      nomes = nomes + "<li>" + row.name + "</li>"
+    });
+    res.send('<h1>Full Cycle</h1> <br/> ' + "<ul>"+nomes+"</ul>")
+    con.end()
+
+  })
+
+})
+
+app.listen(port, ()=> {
+    console.log('Rodando na porta ' + port)
+})
